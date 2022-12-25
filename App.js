5ff4c84f07700282fdcfb6e8845e7d832a95abdd
sravankumar567 <119730950+sravankumar567@users.noreply.github.com>
2022-12-25 11:44:45 +0530

    SetUp files for REACT.js

---
+++ App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+/* eslint eqeqeq: "off", curly: "error" */
+import './App.css';
+import Greet from './components/Hello';
+/* eslint eqeqeq: "off", curly: "error" */
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/classClick';
+import Binging from './components/Binging';
+import ParentEl from './components/ParentEl';
+
+
+
+function App() {
+  return (
+    <div className="App">
+        {/* <Counter></Counter> */}
+      
+        {/* <FunctionClick  /> */}
+        {/* <ClassClick  /> */}
+        {/* <Binging   /> */}
+        <ParentEl  />
+         
+
+        
+        
+          
+    </div>
+  );
+}
+
+export default App;
